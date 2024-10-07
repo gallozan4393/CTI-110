6039e1df6923a3d4a2c4159b10074efc26eed4d0
gallozan4393 <gallozan4393@student.faytechcc.edu>
2024-10-06 23:10:07 -0400

    Add files via upload

---
+++ P1HW1_GallozaN.py
@@ -0,0 +1,30 @@
+#CTI 110
+#Natasha Galloza
+#10/6/2024
+#P1HW1
+
+
+
+print ("P1HW1")
+
+#Exp
+print ("-" * 10, "Calculating Exponents", "-" * 10)
+
+x = input ("Enter an integer as the base value: ")
+y = input ("Enter an integer as the exponent: ")
+result = int(x) ** int(y)
+
+print (x, "raised to the power of", y, "is", result)
+
+#Add,Sub
+print ("-" * 10, "Addition and Subtraction", "-" * 10)
+
+num1 = input ("Enter a starting integer: ")
+num2 = input ("Enter a integer to add: ")
+num3 = input ("Enter a integer to substract: ")
+
+sum_result = int(num1) + int(num2) - int(num3)
+
+print (num1, "+", num2, "-", num3, "is equal to", sum_result)
+
+
