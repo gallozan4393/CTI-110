561da3f6d2f3fcd5d66bd03bf923d4ce1e641e4d
gallozan4393 <gallozan4393@student.faytechcc.edu>
2024-10-06 23:20:44 -0400

    Add files via upload

---
+++ P1LAB1_GallozaN.py
@@ -0,0 +1,13 @@
+#CTI 110
+#Natasha Gallo
+#10/6/2024
+#P1LAB1
+
+
+
+x = input("Enter your first name: ")
+
+y = input("Enter your last name: ")
+
+
+print ("Hello,", x, y,"!", "Welcome to CTI-110")   
